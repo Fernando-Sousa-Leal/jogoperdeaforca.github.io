6dce4d1320f9c2a2553b9a6221dbd8c13a321c40
Fernando-Sousa-Leal <78277616+Fernando-Sousa-Leal@users.noreply.github.com>
2023-08-04 18:44:38 -0300

    Upload do site do portifólio

---
+++ scripts/word-list.js
@@ -0,0 +1,11 @@
+const wordList = [
+    {
+        word: "master",
+        hint: "É o melhor jogador de Outlive?"
+    },
+    {
+        word: "magneto",
+        hint: "É o segundo melhor jogador de Outlive"
+    }
+
+];
